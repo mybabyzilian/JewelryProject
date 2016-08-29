a53c1b324818b50d53e3ccfc6fc898e4c5cb6389
mybabyzilian <472576140@qq.com>
2016-08-29 16:56:43 +0800

    no message

---
+++ Jewelry/Jewelry/Jewelry/app/src/main/java/com/example/admin/jewelry/homepage/jewerydisplay/NewDetailActivity.java
@@ -0,0 +1,24 @@
+package com.example.admin.jewelry.homepage.jewerydisplay;
+
+import com.example.admin.jewelry.R;
+import com.example.admin.jewelry.base.BaseActivity;
+
+/**
+ * Created by admin on 2016/8/29.
+ */
+public class NewDetailActivity extends BaseActivity {
+    @Override
+    public int setLayout() {
+        return R.layout.activity_new_detail;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
