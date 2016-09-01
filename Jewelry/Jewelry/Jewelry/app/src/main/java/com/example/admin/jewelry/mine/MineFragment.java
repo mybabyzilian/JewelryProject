d561ef62052793663f9588dd3a436c59b4fffca1
mybabyzilian <472576140@qq.com>
2016-09-01 15:29:36 +0800

    no message

--- Jewelry/Jewelry/Jewelry/app/src/main/java/com/example/admin/jewelry/mine/MineFragment.java
+++ Jewelry/Jewelry/Jewelry/app/src/main/java/com/example/admin/jewelry/mine/MineFragment.java
@@ -1,21 +1,34 @@
 package com.example.admin.jewelry.mine;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.admin.jewelry.R;
+import com.example.admin.jewelry.Utils.XCRoundImageView;
 import com.example.admin.jewelry.base.BaseFragment;
-import com.example.admin.jewelry.base.MyApp;
 import com.example.admin.jewelry.mine.my_home_page.MyHomePageActivity;
 
 /**
  * Created by admin on 2016/8/15.
  */
 public class MineFragment extends BaseFragment implements View.OnClickListener {
+    private static final int CAPTURE_CODE = 100;
     private ViewPager viewPager;
     private TabLayout tabLayout;
+    private XCRoundImageView cameraIv;
+    private static final int IMAGE_CODE = 101;
+    private Bitmap bm;
+
+
 
 
     @Override
@@ -26,6 +39,8 @@ public class MineFragment extends BaseFragment implements View.OnClickListener {
     @Override
     protected void initView(View view) {
         view.findViewById(R.id.my_home_page_rl).setOnClickListener(this);//我的主页
+        cameraIv = (XCRoundImageView) view.findViewById(R.id.personal_camera);
+        cameraIv.setOnClickListener(this);
 
     }
 
@@ -34,15 +49,49 @@ public class MineFragment extends BaseFragment implements View.OnClickListener {
 
     }
 
+
+
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.my_home_page_rl:
-                Intent intent = new Intent(context,MyHomePageActivity.class);
+                Intent intent = new Intent(context, MyHomePageActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.personal_camera:
+//                Intent takephoto = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(takephoto,CAPTURE_CODE);
+
+                Intent album = new Intent(Intent.ACTION_GET_CONTENT);
+                album.setType("image/*");
+                startActivityForResult(album, IMAGE_CODE);
                 break;
 
         }
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+            ContentResolver resolver = context.getContentResolver();
+            if (resultCode != Activity.RESULT_OK) {
+                return;
+            } else if (requestCode == IMAGE_CODE) {
+                Uri originalUri = data.getData();
+                if (originalUri != null) {
+                    bm = MediaStore.Images.Media.getBitmap(resolver,originalUri);
+                    cameraIv.setImageBitmap(bm);
+                }
+            } else if (requestCode == CAPTURE_CODE && resultCode == Activity.RESULT_OK) {
+                Bundle bundle = data.getExtras();
+                if (bundle != null) {
+                    bm = (Bitmap) bundle.get("data");
+                    cameraIv.setImageBitmap(bm);
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(context, "选择图片错误，图片只能为jpg格式", Toast.LENGTH_SHORT).show();
+        }
     }
 }
